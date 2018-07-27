0613651a0a9f7fdf7495f61174d832183fdd96db
rafaelresende1 <rafael.resende@outlook.com>
2018-07-26 21:48:33 -0300

    1 componnet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { CommonModule } from '@angular/common';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule
